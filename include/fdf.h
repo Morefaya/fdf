8f3a7bfc24a3c2ee32a32a7c80bdea6dceb9a89f
Morefaya <jeancharles.cazako@gmail.com>
2016-05-02 02:59:56 +0200

    vect affine

--- include/fdf.h
+++ include/fdf.h
@@ -4,5 +4,26 @@
 # include "libft.h"
 # include <mlx.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+
+# define X_MAX 1200
+# define Y_MAX 600
+
+# define ESC 53
+
+# define BLUE 0x000000ff
+
+typedef struct	s_pos
+{
+	int	x;
+	int	y;
+}		t_pos;
+
+typedef struct	s_win
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}		t_win;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,60 @@
 #include "fdf.h"
 
+static int	draw(t_win *win)
+{
+	int	i;
+	int	j;
+	t_pos	a;
+	t_pos	b;
+	int	d;
+	int	e;
+
+	a.x = 100;
+	a.y = 200;
+	b.x = 300;
+	b.y = 400;
+
+	d = (a.x - b.x) / (a.y - b.y);
+	e = a.y / (a.x * d);
+	i = a.x;
+	while (i < b.x)
+	{
+		j = d * i + e;
+		mlx_pixel_put(win->mlx_ptr, win->win_ptr, i, j, BLUE);
+		i++;
+	}
+	return (0);
+}
+
+static int	keyboard(int key, t_win *win)
+{
+	if (key == 53)
+	{
+		mlx_destroy_window(win->mlx_ptr, win->win_ptr);
+		exit(0);
+	}
+	else
+		ft_putnbr(key);
+		ft_putchar('\n');	
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*ptr_win;
+	t_win	win;
 
-	mlx_ptr = NULL;
-	ptr_win = NULL;
-	if (!(mlx_ptr = mlx_init()))
+	if (!(win.mlx_ptr = mlx_init()))
+	{
 		ft_putstr("error init");
-	if (!(ptr_win = mlx_new_window(mlx_ptr, 1000, 1000, "yolo")))
+		return (1);
+	}
+	if (!(win.win_ptr = mlx_new_window(win.mlx_ptr, X_MAX, Y_MAX, "yolo")))
+	{
 		ft_putstr("error new window");
+		return (2);
+	}
+	mlx_expose_hook(win.win_ptr, draw, &win);
+	mlx_key_hook(win.win_ptr, keyboard, &win);
+	mlx_loop(win.mlx_ptr);
 	return (0);
 }
